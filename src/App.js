21b0ed907007241d2ad1447c469116cc0442526c
Julian Camilo <julian20141016@gmail.com>
2020-11-30 18:57:45 -0500

    Deleted project

--- src/App.js
+++ src/App.js
@@ -1,27 +1,12 @@
-import { createStore } from "redux";
-import todoApp from "./redux/reducers";
-import {
-  addTodo,
-  toggleTodo,
-  setVisibilityFilter,
-  VisibilityFilters,
-} from "./redux/actions";
-let store = createStore(todoApp);
+import React from "react";
+import { Provider } from "react-redux";
+import ToDoList from "./ToDoList";
+import Store from './redux/store'
 
 export default function App() {
-  console.log(store.getState());
-
-  let unsubscribe = store.subscribe(() => console.log(store.getState()));
-
-  store.dispatch(addTodo("Aprender sobre acciones"));
-  store.dispatch(addTodo("Aprender sobre reductores"));
-  store.dispatch(addTodo("Aprender sobre stores"));
-  store.dispatch(toggleTodo(0));
-  store.dispatch(toggleTodo(1));
-  store.dispatch(setVisibilityFilter(VisibilityFilters.SHOW_COMPLETED));
-
-  // Anulamos el monitoreo de las actualizaciones al estado
-  unsubscribe();
-
-  return <div>Redux Concepts</div>;
+  return (
+    <Provider store={Store}>
+      <ToDoList />
+    </Provider>
+  );
 }
